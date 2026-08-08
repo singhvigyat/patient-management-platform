115e79a8f5bf0758ac2283566daa22e4c05ed999
singhvigyat <code.vigyat@gmail.com>
2026-08-08 17:25:04 +0530

    add getters & setters to private members in patient model

--- patient-service/src/main/java/com/example/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/example/patientservice/model/Patient.java
@@ -30,4 +30,53 @@ public class Patient {
     @NotNull
     private LocalDate registeredDate;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+
 }
