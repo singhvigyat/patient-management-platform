64ef7f0f95a64d069101321de5cc7a5e2baab0cf
singhvigyat <code.vigyat@gmail.com>
2026-08-24 00:12:32 +0530

    setup kafka patient service configuration

--- patient-service/src/main/java/com/example/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/example/patientservice/kafka/KafkaProducer.java
@@ -1,14 +1,35 @@
 package com.example.patientservice.kafka;
 
+import com.example.patientservice.model.Patient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
 
 @Service
 public class KafkaProducer {
+    private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, byte[]>kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]>kafkaTemplate){
         this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEvent(Patient patient){
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED") // to add subcategory to a message
+                .build();
+
+        try{
+            kafkaTemplate.send("patient",event.toByteArray()) ;
+        }catch(Exception e){
+            log.error("Error Sending PatientCreated event: {}", event);
+        }
+
     }
 
 
